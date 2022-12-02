a7c44ef9652ba5b1c1f93316e0d743f22cae497c
sacred70 <sacred70@mail.ru>
2022-12-02 13:25:02 +0700

    1

--- utils.py
+++ utils.py
@@ -1,6 +1,20 @@
+# coding: utf8
+import json
+
+
+def load_candidates(file="candidates.json"):
+    # загрузит данные из файла
+    with open(file, "r", encoding='utf-8') as f:
+        text = json.load(f)
+        return text
+
+
+#print(load_candidates())
+
+
 def load_candidates_from_json(path):
-    #  озвращает список всех кандидатов
-    pass
+    #  возвращает список всех кандидатов
+    list_cand = f'< h1 > Все кандидаты < / h1 >\n'
 
 
 def get_candidate(candidate_id):
