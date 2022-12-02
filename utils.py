57e04731a6ad8857770e1003aa58ee549bfd213b
sacred70 <sacred70@ubuntu.myguest.virtualbox.org>
2022-12-02 10:45:24 +0700

    Initial commit

---
+++ utils.py
@@ -0,0 +1,18 @@
+def load_candidates_from_json(path):
+    #  озвращает список всех кандидатов
+    pass
+
+
+def get_candidate(candidate_id):
+    # возвращает одного кандидата по его id
+    pass
+
+
+def get_candidates_by_name(candidate_name):
+    # возвращает кандидатов по имени
+    pass
+
+def get_candidates_by_skill(skill_name):
+    #  возвращает кандидатов по навыку
+    pass
+
