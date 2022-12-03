99627a186598db7b356a2545e375cdeedf0aa7a2
sacred70 <sacred70@mail.ru>
2022-12-03 09:25:01 +0700

    final

--- utils.py
+++ utils.py
@@ -7,48 +7,30 @@ def load_candidates(file="candidates.json"):
     with open(file, "r", encoding='utf-8') as f:
         text = json.load(f)
         return text
-#print(load_candidates())
 
 
 def load_candidates_from_json(path=load_candidates()):
     #  возвращает список всех кандидатов
-    list_cand ={}
+    list_cand = {}
     for cand in load_candidates():
-        list_cand[cand["id"]] =cand["name"]
+        list_cand[cand["id"]] = cand["name"]
     return list_cand
-#print(load_candidates_from_json())
-#for r in load_candidates_from_json():
-    #print(r, load_candidates_from_json()[r])
-    #print()
+
 
 def get_candidate(candidate_id):
-    # возвращает одного кандидата по его id
+    #  возвращает одного кандидата по его id
     for cand in load_candidates():
         if cand['id'] == candidate_id:
-            #name = cand["name"]
-            #position = cand["position"]
-            #picture = cand["picture"]
-            #skills = cand["skills"]
-
-
-            return cand #name, position, picture, skills
-
-#print(get_candidate(1)[0])
+            return cand
 
 
 def get_candidates_by_name(candidate_name):
     # возвращает кандидатов по имени
-
     list_cand = {}
     for cand in load_candidates():
         if candidate_name.lower() in cand['name'].lower():
             list_cand[cand["id"]] = cand["name"]
     return list_cand
-#print(get_candidates_by_name("Adela"))
-
-
-
-
 
 
 def get_candidates_by_skill(skill_name):
@@ -59,4 +41,4 @@ def get_candidates_by_skill(skill_name):
         if skill in cand['skills'].lower():
             list_cand[cand["id"]] = cand["name"]
     return list_cand
-print(get_candidates_by_skill("PYthon"))
+
